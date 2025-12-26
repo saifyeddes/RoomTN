12769fa98d8559eb4459dfdf50deb5cab14467f1
saifyedes <saifyedes20@gmail.com>
2025-12-26 12:39:24 +0100

    fix server routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 
-// ✅ OBLIGATOIRE
+// ✅ JSON
 app.use(express.json());
 
 // ✅ CORS
@@ -12,20 +12,17 @@ app.use(cors({
     'https://tnroom.netlify.app',
     'http://localhost:5173'
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
 }));
 
+// ✅ STATIC (images)
+app.use('/uploads', express.static('uploads'));
 
 // ✅ ROUTES
-const authRoutes = require('./routes/auth');
-app.use('/api/auth', authRoutes);
-// ✅ ROUTES
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api', require('./routes/adminStats'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
+app.use('/api', require('./routes/adminStats'));
 
 // ✅ TEST
 app.get('/', (req, res) => {
